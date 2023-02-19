2666dcc2733694037502279190662ba209319538
jkkdfb <jkkdfb894@gmail.com>
2023-02-19 17:18:43 +0800

    master commit1

---
+++ git-test01/src/main/java/com/auguigu/Test.java
@@ -0,0 +1,13 @@
+package com.auguigu;
+
+/**
+ * @author: 友人A
+ * @className: Test
+ * @version: 1.0
+ * @date: 2023/2/19 17:17
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("master分支01");
+    }
+}
